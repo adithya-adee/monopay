79ecd38d2164e3e6981f20739abe64f8c9947a06
adithya-adee <adithya25905@gmail.com>
2026-03-29 13:45:34 +0530

    feat: add verbose security lifecycle logging for dev audit

---
+++ advanced-vault-mock/vault-ui/src/wasm/wasm_vault.d.ts
@@ -0,0 +1,66 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function derive_key(password: string, salt: string): Uint8Array;
+
+/**
+ * Generate 12 random words from the BIP-39 list using the bip39 crate.
+ */
+export function generate_mnemonic(): string;
+
+/**
+ * Generate a secure 16-character Salt for Argon2id
+ */
+export function generate_salt(): string;
+
+/**
+ * Securely zero all sensitive internal buffers.
+ */
+export function lock_vault(): void;
+
+/**
+ * Sign a "transaction" hash using the internal key
+ * Returns a mock 64-byte signature
+ */
+export function secure_sign(tx_hash: Uint8Array): Uint8Array;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly derive_key: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+    readonly generate_mnemonic: () => [number, number];
+    readonly generate_salt: () => [number, number];
+    readonly lock_vault: () => void;
+    readonly secure_sign: (a: number, b: number) => [number, number, number, number];
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ advanced-vault-mock/vault-ui/src/wasm/wasm_vault_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const derive_key: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const generate_mnemonic: () => [number, number];
+export const generate_salt: () => [number, number];
+export const lock_vault: () => void;
+export const secure_sign: (a: number, b: number) => [number, number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
